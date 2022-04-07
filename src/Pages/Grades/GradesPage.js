17a56ba5412104b681ed8766c491e175d5acbc98
steveng85 <s205456@student.dtu.dk>
2022-04-07 19:46:23 +0200

    made data handling for grades

--- src/Pages/Grades/GradesPage.js
+++ src/Pages/Grades/GradesPage.js
@@ -1,7 +1,57 @@
+
+import { useState, useEffect } from "react";
+import Grades from "./Components/Grades";
+
 const GradesPage = () => {
-    return (
-      <p>Her kommer  Grades :)</p>
-    )
-  };
-  
-  export default  GradesPage;
+const [gradesData, setGradeesData] = useState([])
+
+useEffect(() => {
+  getGrades();
+}, []) 
+
+
+async function getGrades() {
+
+  try {
+
+    setTimeout(() => {
+
+      let grades = [{
+        id: 1,
+        courseId: 62332,
+        courseName: "Compiler",
+        grade: 7,
+        ETCS: 5
+      },
+      {
+        id: 2,
+        courseId: 62577,
+        courseName: "Data Koomunikation",
+        grade: 10,
+        ETCS: 5
+      }]
+
+      setGradeesData(grades)
+
+
+    }, 1000)
+  } catch (error){
+    console.log(error)
+  }
+}
+
+function loadGrades() {
+  return gradesData.map(data => <Grades key={data.id}  {...data} />)
+}
+
+
+  return (
+    <div>
+      {loadGrades()}
+    </div>
+    
+  )
+};
+
+
+export default GradesPage;
