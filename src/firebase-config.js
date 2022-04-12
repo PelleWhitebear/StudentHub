baa6b315355aeaf0e6d005e5535f6335ab66c45f
Mia <miadong01@yahoo.com>
2022-04-12 13:25:35 +0200

    firebase config

---
+++ src/firebase-config.js
@@ -0,0 +1,18 @@
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCJtckBTE3-ub4JP6NcEJX_PKao7r0YJRw",
+    authDomain: "dtustudenthub.firebaseapp.com",
+    projectId: "dtustudenthub",
+    storageBucket: "dtustudenthub.appspot.com",
+    messagingSenderId: "400034264848",
+    appId: "1:400034264848:web:f065a4bb76463063dd5795",
+    measurementId: "G-M5K2EJKLEL"
+  };
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+export const auth = getAuth(app)
