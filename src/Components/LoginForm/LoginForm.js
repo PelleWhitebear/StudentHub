3f7d2dd1b019afe5d6421568b534f59395d01454
Mia <miadong01@yahoo.com>
2022-04-12 13:25:17 +0200

    added firebase login

--- src/Components/LoginForm/LoginForm.js
+++ src/Components/LoginForm/LoginForm.js
@@ -1,25 +1,131 @@
 import { Link } from 'react-router-dom';
 import './LoginForm.css';
+import { useState } from 'react';
+import { 
+    signInWithEmailAndPassword,
+    createUserWithEmailAndPassword, 
+    onAuthStateChanged,
+    signOut
+} from 'firebase/auth'
+import { auth } from '../../firebase-config.js';
+
 
 const LoginForm = () => {
+    const [registerEmail, setRegisterEmail] = useState("");
+    const[registerPassword, setRegisterPassword] = useState("");
+    const [loginEmail, setLoginEmail] = useState("");
+    const[loginPassword, setLoginPassword] = useState("");
+    const [user, setUser] = useState({}); 
+
+    onAuthStateChanged(auth, (currentUser) => {
+        setUser(currentUser);
+    })
+  
+    const register = async () => {
+        try {
+            const user = await createUserWithEmailAndPassword(
+                auth, registerEmail, registerPassword
+            );
+        } catch (error) {
+            console.log(error.message);
+        }
+    }
+
+    const login = async () => {
+        try {
+            const user = await signInWithEmailAndPassword(
+                auth, loginEmail, loginPassword
+            );
+        } catch (error) {
+            console.log(error.message);
+        }
+    }
+    
+    const logout = async () => {
+        await signOut(auth);
+    }
+
     return (
+        
+        <>
         <form action="#"> 
             <div>
                 <span>
-                    <input className='InputBox' type="text" placeholder="Student-ID"></input>
+                    <input 
+                    className='InputBox' 
+                    type="text" 
+                    placeholder="Student mail" 
+                    onChange={(e) => setLoginEmail(e.target.value)}
+                    ></input>
                 </span>
             </div>
             <div>
                 <span>
-                    <input className='InputBox' type="password" placeholder="Password"></input>
+                    <input 
+                    className='InputBox' 
+                    type="password" 
+                    placeholder="Password"
+                    onChange={(e) => setLoginPassword(e.target.value)}
+                    ></input>
                 </span>
             </div>
             <div>
                 <Link to="/Calendar">
-                    <button className='LoginButton'>Login</button>
+                    <button 
+                    className='LoginButton'
+                    onClick={login}
+                    >Login</button>
                 </Link>
             </div>
+          
+            <div>
+                <Link to="/Calendar">
+                    <button 
+                    className='CreateUserButton'
+                    onClick={logout}
+                    >Logout</button>
+                </Link>
+            </div>
+        
         </form>
+        <div>
+            <p> {user?.email} </p>
+        </div>
+        <form action="#"> 
+            <div>
+                <span>
+                    <input 
+                    className='InputBox' 
+                    type="text" 
+                    placeholder="Student mail" 
+                    onChange={(e) => setRegisterEmail(e.target.value)}></input>
+                </span>
+            </div>
+            <div>
+                <span>
+                    <input 
+                    className='InputBox' 
+                    type="password" 
+                    placeholder="Password"
+                    onChange={(e) => setRegisterPassword(e.target.value)}
+                    ></input>
+                </span>
+            </div>
+            <div>
+                <Link to="/Calendar">
+                    <button 
+                    className='CreateUserButton'
+                    onClick={register}
+                    >Create User</button>
+                </Link>
+            </div>
+
+ 
+        
+        </form>
+        
+        </>
+        
     )
   };
   
