ea34b5eeeb8da09829c0798658e7ba6c04d35bfd
steveng85 <s205456@student.dtu.dk>
2022-04-15 12:07:15 +0200

    h

--- src/Pages/Announcements/AnnouncementPage.js
+++ src/Pages/Announcements/AnnouncementPage.js
@@ -1,7 +1,7 @@
 import { useParams } from "react-router-dom";
 import "./Styles/Announcement.css";
 import { useEffect, useState } from "react";
-import axios from ('axios')
+import axios from 'axios';
 
 const AnnouncementPage = () => {
     const { id } = useParams();
