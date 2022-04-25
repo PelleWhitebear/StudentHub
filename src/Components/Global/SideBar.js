c777d6ce46da7545c950b1fbe7c8ea42d094841a
Mia <miadong01@yahoo.com>
2022-04-25 22:15:32 +0200

    add to courses

--- src/Components/Global/SideBar.js
+++ src/Components/Global/SideBar.js
@@ -1,20 +1,29 @@
 import { Checkbox, FormControlLabel, Stack } from "@mui/material";
 import "./Styles/SideBar.css";
-import SymbolButton from "./SymbolButton";
 import AddAppointment from "../../Pages/Calendar/components/addAppointment";
+import SymbolButton from './SymbolButton'
+import '../LoginForm/LoginForm.css'
 
+const InputField = (props) => {
+  return (
+    <input onChange={props.onChange} type="input"></input>
+    
+  );
+
+};
 const SideBar = (props) => {
+
   return (
     <>
       <div className="SideBar">
         <br />
         <br />
         <AddAppointment />
-        <Stack direction="column" spacing={4}>
+        <Stack direction="column" spacing={2}>
           <br />
           <br />
           <h3>My Calendars</h3>
-          {props?.courses?.map((element) => {
+          {props?.data?.map((element) => {
             return (
               <Stack key={props?.courses?.indexOf(element)}>
                 <FormControlLabel
@@ -25,7 +34,10 @@ const SideBar = (props) => {
             );
           })}
         </Stack>
-        <SymbolButton 
+        <InputField
+        onChange={props.onChange} />
+        <SymbolButton
+        onClick={props.onClick}
         symbol={props.symbol} />
       </div>
     </>
--- src/Components/Global/SymbolButton.js
+++ src/Components/Global/SymbolButton.js
@@ -1,11 +1,12 @@
-import "./Styles/SymbolButton.css";
+import './Styles/SymbolButton.css'
 const SymbolButton = (props) => {
   return (
     <>
       <br />
-      <button className="addButton">{props.symbol}</button>
+      <button onClick={props.onClick} className="addButton">{props.symbol}</button>
       <br />
     </>
   );
 };
+
 export default SymbolButton;
