538461990150b6b30fb5d513356a6f12ba937c08
s205345 <sebassinding@gmail.com>
2022-05-09 16:43:23 +0200

    text to addappointments

--- src/views/StudyGuidance/StudyGuidancePage.js
+++ src/views/StudyGuidance/StudyGuidancePage.js
@@ -2,6 +2,8 @@ import './Styles/StudyGuidance.css';
 import emailjs from "emailjs-com"
 import InputField from '../../components/Global/InputField';
 import AddAppointment from '../../components/Global/addAppointment'
+import { Container } from '@mui/material';
+import { Button, Title } from '../../index'
 
 const StudyGuidance = () => {
 
@@ -18,27 +20,28 @@ const StudyGuidance = () => {
 }
 
     return (
-    <div>
-     <div>
-       <form onSubmit={sendEmail}>      
-         <div className="containerInputFields">
-           <InputField placeholder="Subject" name="Subject"></InputField>
-        </div>
-        <div className='containerInputFields'>
-          <InputField type="email" placeholder="Enter your email for reply" name="reply_email"></InputField>
-        </div>
-        <div>
-          <textarea className="textareaMessage" placeholder="Your message" name="message"></textarea>
-        </div>
-        <div className="inputButton">
-          <InputField type="submit" value="Send Message" name="reply_email"></InputField>
-        </div>
-      </form>
-    </div>
-    <div className='bookMettingBtn'>
-      <AddAppointment addAppointmentText='Book Meeting' buttonText='Book Meeting'/>
-    </div>
-    </div>
+      <div className='containerpage'>
+        <form onSubmit={sendEmail}>
+          <Title title="StudyGuidance" />  
+          <div className='containerBookMeetingBtn'>
+            <AddAppointment addAppointmentText='Book Meeting' buttonText='Book Meeting'/>
+          </div>
+          <div className="containerInputFields">
+            <InputField placeholder="Subject" name="Subject"></InputField>
+          </div>
+          <div className='containerInputFields'>
+            <InputField type="email" placeholder="Enter your email for reply" name="reply_email"></InputField>
+          </div>
+          <div className='containerTextarea'>
+            <textarea className="textareaMessage" placeholder="Your message" name="message"></textarea>
+          </div>
+          <div className="containerInputButton">
+            <Button buttonText="Send mail">
+              <InputField className='InputButton' type="submit" value="Send Message" name="reply_email"></InputField>
+            </Button>
+          </div>
+        </form>
+      </div>
     )
   }
   ;
